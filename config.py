3f0778340c47b6b3b8cc3ad23cafc3653c593715
Stefan <bekli23@yahoo.com>
2025-04-07 15:20:45 +0100

    Add files via upload
    Puzzle 68 was found by a mysterious user, I'm not 100% sure if he used my script, you need to update config.py for puzzle 69

--- config.py
+++ config.py
@@ -1,12 +1,11 @@
 CONFIG = {
-    "address": "1MVDYgVaSN6iKKEsbzRUAYFrYJadLYZvvZ",
-    "range_start": "80000000000000000",
-    "range_end": "fffffffffffffffff",
+    "address": "19vkiEajfhuZ8bs8Zu2jgmC6oqZbWqhxhG",
+    "range_start": "100000000000000000",
+    "range_end": "1fffffffffffffffff",
     "range_bits": 33,
-    "gpu_ids": [
-        0
-    ],
-    "gpu_threads": "512,512",
+    "gpu_ids": [0],
+    "gpu_grids": ["256", "256"],
+    "gpu_threads": 512,
     "cpu_threads": 4,
     "mode": "address",
     "coin": "BTC",
